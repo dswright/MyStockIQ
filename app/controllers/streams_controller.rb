11556a21071db2da592d3f9d927ff13c81aa8bf9
seanmerrifield <seanmerrifield@gmail.com>
2014-11-18 23:21:20 -0800

    Setup stream posting on User Page

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -4,8 +4,27 @@ class StreamsController < ApplicationController
 	before_action :logged_in_user
 
 	def create
+		#Obtain user session information from Session Helper function 'current_user'.
+		@current_user = current_user
+
+		#Builds 'Streams' object related to current user. This syntax is only required when looking up the 'Streams' model by 'User'
+		@post = current_user.streams.build(stream_params)
+
+		if @post.save
+			flash[:success] = "Post created!"
+			redirect_to root_url
+		else
+			redirect_to root_url
+		end
 	end
 
 	def destroy
 	end
+
+	private
+
+		def stream_params
+			params.require(:stream).permit(:content)
+		end
 end
+
