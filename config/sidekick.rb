0c35dc56626d2096e07da373a8349a14a56e7c54
Dylan <dylansamuelwright@gmail.com>
2014-11-24 18:29:13 -0800

    sidekiq update

--- config/sidekick.rb
+++ config/sidekick.rb
@@ -1,10 +1,10 @@
 require 'redis'
-REDIS = Redis.new(host: "redis://localhost", port: 15255)
+REDIS = Redis.new(host: "redis://localhost", port: 9250)
  
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://localhost:15255" }
+  config.redis = { url: "redis://localhost:9250" }
 end
  
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:15255" }
+  config.redis = { url: "redis://localhost:9250" }
 end
