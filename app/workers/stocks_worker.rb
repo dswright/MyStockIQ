523f63674bb6ef12f6bb44742cc78de9846792fe
Dylan <dylansamuelwright@gmail.com>
2014-11-26 14:45:08 -0800

    updated workers setup

--- app/workers/stocks_worker.rb
+++ app/workers/stocks_worker.rb
@@ -6,13 +6,21 @@ class StocksWorker
     #Stock.fetch_new_stocks pulls the 28 pages of stock tickers from Quandl and checks for any new ticker symbols.
     #Saves new tickers to the db.
     #Returns list of inserted and failed inserts to the stock_array.
-    stock_array = Stock.fetch_new_stocks
+    #stock_array = Stock.fetch_new_stocks
     #Takes the array of inserted stock objects and updates them with the correct stock industry.
     #Returns list of updated stock objects to the stock_array.
-    stock_array[:inserted] = Stock.return_industry_array(@stock_array[:inserted])
+    #stock_array[:inserted] = Stock.return_industry_array(@stock_array[:inserted])
     #additionally update the inserted values with the correct pe ratio.
-    stock_array[:inserted] = Stock.return_pe(@stock_array[:inserted])
+    #stock_array[:inserted] = Stock.return_pe(@stock_array[:inserted])
     #Sends an email with the array of failed and inserted stocks.
-    StockMailer.new_stocks(@stock_array).deliver_now
+    #StockMailer.new_stocks(@stock_array).deliver_now
+
+    Stock.delete_all
+    x = 28
+    x.times do |i|
+      # set data_set_docs = to the "docs" array of objects from the quandl json file
+      QuandlWorker.perform_async(i)
+    end
+    #retun the saved and inserted stock_array arrays.
   end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,6 +17,8 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool']            =   ENV['DB_POOL']
+    ActiveRecord::Base.establish_connection(config)
 end
