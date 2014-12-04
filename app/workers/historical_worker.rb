8cb8c9cc862d7c168f6df1f106acc9fef74c17e9
Dylan <dylansamuelwright@gmail.com>
2014-12-04 12:22:39 -0800

    updated off by 1

--- app/workers/historical_worker.rb
+++ app/workers/historical_worker.rb
@@ -6,18 +6,18 @@ class HistoricalWorker
   require 'json'
 
   def perform(stock_hash_array, count, i)
-    unless stock_hash_array[count].nil?
+    #unless stock_hash_array[count].nil?
       ticker_symbol = stock_hash_array[count]["ticker_symbol"]
       unless ticker_symbol.nil?
         Stockprice.fetch_new_prices(ticker_symbol)
       end
 
-      if count < stock_hash_array.count
+      if count + 1 < stock_hash_array.count
         HistoricalWorker.perform_async(stock_hash_array, count+1, i)
       else
         QuandlWorker.perform_async(i+1)
       end
-    end
+    #end
   end
 
 end
