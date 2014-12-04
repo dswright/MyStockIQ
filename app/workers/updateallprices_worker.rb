e9e22e839d991beb94a5deb85e4a18e866db1f4e
Dylan <dylansamuelwright@gmail.com>
2014-12-04 15:29:05 -0800

    scaled to 30

--- app/workers/updateallprices_worker.rb
+++ app/workers/updateallprices_worker.rb
@@ -1,12 +1,10 @@
 class PricesupdateWorker
   include Sidekiq::Worker
   require 'date'
-  sidekiq_options timeout: 60
 
   def perform
-    stock = Stock.where("updated_at <= ?", DateTime.now.to_date+1).where(active:true).first
+    stock = Stock.where("updated_at <= ?", DateTime.now.to_date).where(active:true).first
     
     PricesupdateWorker.perform_async(stock.ticker_symbol)
-    end
   end
 end
