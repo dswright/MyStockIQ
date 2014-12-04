27f06e3adebbf1101d6334a825ee7c386ae06969
Dylan <dylansamuelwright@gmail.com>
2014-12-04 08:47:48 -0800

    unscaled

--- app/workers/quandl_worker.rb
+++ app/workers/quandl_worker.rb
@@ -6,7 +6,7 @@ class QuandlWorker
   require 'json'
 
   def perform(i)
-    if i<=30
+    if i<=1
       stock_hash_array = []
       #set url = to the quandl json url.
       url = "http://www.quandl.com/api/v2/datasets.json?source_code=EOD&per_page=300&page=#{i}&auth_token=sVaP2d6ACjxmP-jM_6V-"
