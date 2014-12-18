a7df10a77f4cc0480d0534dba8176a55f4884ec0
Dylan <dylansamuelwright@gmail.com>
2014-12-17 19:56:04 -0800

    droped schedule.rb

--- app/controllers/stockprices_controller.rb
+++ app/controllers/stockprices_controller.rb
@@ -2,18 +2,5 @@ class StockpricesController < ApplicationController
   
 
   #this function runs once to harvest 5 years of data
-  def create
-    stocks = Stock.where(date:nil, active:true)
-      
-    stocks.each do |stock|
-      HistoricalWorker.perform_async(stock.ticker_symbol)
-    end
-  end
-
-  def update
-    stocks = Stock.where(active:true)
-    stocks.each do |stock|
-      LatestWorker.perform_async(stock.ticker_symbol)
-    end
-  end
+  
 end
