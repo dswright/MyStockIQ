28722bcc3d66b4cb7ad95b1d9ee9da10da01bb17
Dylan <dylansamuelwright@gmail.com>
2014-12-18 19:31:43 -0800

    Added Highcharts js stock files

---
+++ app/assets/javascripts/testing.js
@@ -0,0 +1,8 @@
+
+//so some basic js is created here.. what can i do with it..? It's precompiled.. but I also need it to use ruby data..
+//maybe make the js accept ruby params that are passed in to it?
+
+//this gets the element by id demo and changes the text in that demo... incredible. Make sure to use the innerhtml method..
+function changeText() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
