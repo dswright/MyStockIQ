608e71107284f0819db1be03e2bd6530ca5242be
seanmerrifield <seanmerrifield@gmail.com>
2014-12-21 14:04:45 -0800

    Modified Comments Table

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
   post     'comments'                     =>   'comments#create'
   delete   'comments/:id'                 =>   'comments#destroy'
 
-  post     'predicions'                   =>    'predictions@create'
+  #post     'predicions'                   =>    'predictions@create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
