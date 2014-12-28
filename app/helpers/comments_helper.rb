5108f286f655547d67185a492efd38934d804fce
seanmerrifield <seanmerrifield@gmail.com>
2014-12-28 11:56:46 -1000

    Fixed comment helper function

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,24 @@
 module CommentsHelper
 
-	def stock_or_user_page(stream_item_one)
-		if stream_item_one[:target_type] == "stock"
-			"/stocks/#{stream_item_one[:target_id]}/"
+	def stock_or_user_page(stream)
 
-		elsif stream_item_one[:target_type] == "user"
-			"/users/#{stream_item_one[:target_id]}/"
+		#If post was made on stock page, redirect to stock page
+		if stream.target_type == "stock"
+			stock = Stock.find_by(id: stream.target_id)
+			"/stocks/#{stock.ticker_symbol}/"
+
+		#If post was made on user page, redirect to user page
+		elsif stream.target_type == "user"
+			user = User.find_by(id: stream.target_id)
+			"/users/#{user.username}/"
+		end
+	end
+
+	def stock_or_user_view(stream)
+		if stream.target_type == "stock"
+			"/stocks/show"
+		elsif stream.target_type == "user"
+			"/users/show"
 		end
 	end
 end
