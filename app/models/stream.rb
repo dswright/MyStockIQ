39fc4288b30c0d441fa06450fd9a13a12c6e658b
Dylan <dylansamuelwright@gmail.com>
2014-12-30 18:21:40 -0800

    created stream function outline

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -7,7 +7,6 @@ class Stream < ActiveRecord::Base
   #uses Rails default_scope function to sort the posts such that the most recent one is first.
   default_scope -> {order('created_at DESC')}
 
-
   ######### STREAM MODEL VALIDATIONS ##########
 
   #Stream.valid? returns TRUE when:
@@ -17,4 +16,24 @@ class Stream < ActiveRecord::Base
 
   	#content exists and has a max length of 140 characters
   	#validates :content, presence: true, length: {maximum: 140}
+
+
+   def self.stream_maker(stream, indent_count)
+    {
+      stream_array = []
+      stream.each do |stream_item|
+        #begin making the array immediately..
+
+        #pull from the stream where the target item is the id of the current stream item.
+        
+
+
+      #each comment will be an array?
+
+
+      #for now make a maximum of 5 recursions... per comment. But modify that laster. Must be limited more intelligently than that later.
+      return stream_array
+
+    }
+
 end
