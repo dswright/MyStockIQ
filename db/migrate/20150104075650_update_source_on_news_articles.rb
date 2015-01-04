76e8e0eb81485d8bdb176bdf8658ea6f56325459
Dylan <dylansamuelwright@gmail.com>
2015-01-03 23:59:16 -0800

    newsarticle source updated

--- db/migrate/20150104075650_update_source_on_news_articles.rb
+++ db/migrate/20150104075650_update_source_on_news_articles.rb
@@ -1,4 +1,4 @@
-class AddSourceToNewsarticles < ActiveRecord::Migration
+class UpdateSourceOnNewsArticles < ActiveRecord::Migration
   def change
     remove_column :newsarticles, :soruce, :string
     add_column :newsarticles, :source, :string
