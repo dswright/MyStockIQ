fe17165cb1484deed6f047de48d4e329e419dd9d
Dylan <dylansamuelwright@gmail.com>
2015-02-25 11:35:06 -0800

    updated tooltip date

--- app/assets/javascripts/stocks.coffee.js
+++ app/assets/javascripts/stocks.coffee.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
             $('#predictiondetailsbox').html(data).fadeIn("slow"); //this loads in the html returned from the ajax request.
           })
           var niceDate = this.x;
-          niceDate = niceDate.utcTimeInt().utcTimeStr().utcTime().utcTimeStr();
+          niceDate = niceDate.utcTimeInt().utcTimeStr();
           return niceDate + ': $' + this.y;
         }
         else {
