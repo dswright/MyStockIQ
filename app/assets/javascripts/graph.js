98ffe6bd26c0d4308d47a29f2ce1a0bc45f6be64
Dylan <dylansamuelwright@gmail.com>
2015-02-26 10:06:00 -0800

    updated graph my_prediction

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -262,7 +262,7 @@ function ChartFunctions(graph, chart) {
   }
 
   function updateMyPrediction (button) {
-    if (button === "1d" || button === "5d") && (currentRange["buttonType"] !== "1d" && currentRange["buttonType"] !== "5d") {
+    if ((button === "1d" || button === "5d") && (currentRange["buttonType"] !== "1d" && currentRange["buttonType"] !== "5d")) {
       chart.series[3].setData(graph["intraday_my_prediction"]);
     }
     if ((button !== "1d" && button !== "5d") && (currentRange["buttonType"] === "1d" || currentRange["buttonType"] === "5d")) {
