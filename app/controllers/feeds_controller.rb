0748fb500aecec1fdce0688a968fa87ca7903655
Dylan <dylansamuelwright@gmail.com>
2015-03-06 21:01:15 -0800

    updated history sort

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -14,7 +14,7 @@ class FeedsController < ApplicationController
     @predictions = @current_user.predictions.where(active:true)
 
     #sets @predictions for the view, and for making stream.
-    @historical_predictions = @current_user.predictions.where(active:false).order(score: :desc).limit(5)
+    @historical_predictions = @current_user.predictions.where(active:false).order(score: :desc)
 
 
     #this is the tricky line. The stream needs to be build well.
