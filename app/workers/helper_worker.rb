a0c42749e5c3ec2903994120c85f2db3d50a385b
Dylan <dylansamuelwright@gmail.com>
2015-03-12 11:00:46 -0700

    daily percent change update

--- app/workers/helper_worker.rb
+++ app/workers/helper_worker.rb
@@ -6,13 +6,28 @@ class HelperWorker
 
   def perform(ticker_symbol)
     stockprices = Stockprice.where(ticker_symbol:ticker_symbol).reorder("date Desc").limit(1501)
-    stockprices.each_with_index do |stockprice, index|
-      unless index == stockprices.count - 1 #if this is the last row of data, dont run the update.
-        previous_price = stockprices[index+1].close_price
+    case_lines = []
+    stockprices[0..-2].each_with_index do |stockprice, index|
+      previous_price = stockprices[index+1].close_price
+      if previous_price == 0
+        daily_percent_change = 0
+      else
         daily_percent_change = ((stockprice.close_price/previous_price -1)*100).round(2)
-        stockprice.daily_percent_change = daily_percent_change
-        stockprice.save
       end
+      case_lines << "WHEN date = '#{stockprice.date}' THEN #{daily_percent_change}"
+    end
+    unless case_lines.empty?
+      sql = "update stockprices
+              SET daily_percent_change = CASE
+                #{case_lines.join("\n")}
+              END
+            WHERE ticker_symbol = '#{ticker_symbol}';" #this is the sql shell that runs. Its contents are based on its 2 arrays.
+      ActiveRecord::Base.connection.execute(sql) #this executes the raw sql.
     end
   end
 end
+
+
+
+
+
