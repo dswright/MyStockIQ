3d03c31c3beda96618b5b979f13bedae5cfeaf06
danajwright <danajwright@gmail.com>
2015-03-14 20:36:27 -0700

    new open prediction sidebar stuff on stock page, plus prediction-form.js

---
+++ app/assets/javascripts/prediction-form.js
@@ -0,0 +1,15 @@
+
+	alert("Hello! I am an alert box!!");
+
+
+$(document).ready(function() {
+
+
+
+  $('#clickthis').click(function() {
+    alert("Hello! I am an alert box!!");
+    $('#toggle').toggle('slow');
+  });
+});
+
+console.log("testing")
