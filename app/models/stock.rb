7a92714d268204db03ab67436ecdbe017db4ebb6
Sean Merrifield <seanmerrifield@gmail.com>
2015-03-17 00:23:48 -0700

    Stock Top Analysts

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,14 +4,20 @@ class Stock < ActiveRecord::Base
 
   has_many :streams, as: :targetable, dependent: :destroy
   has_many :predictions
-  
-  validates :stock,         presence: true
+  has_many :users, through: :predictions
 
+  validates :stock,         presence: true
   validates :ticker_symbol, presence: true,
             uniqueness: {case_sensitive: false}
+
   
   def followers
     Relationship.where(followed_id: self.id, followed_type: self.class.name)
+  end
+
+  def top_analysts(top=100)
+  	sorted_analysts = self.users.uniq.sort_by{|user| user.total_score(self)}
+  	sorted_analysts.reverse.first(top)
   end
 end
 
