530c8292c233fce5c7881386404a1bcad1f4b2b8
Dylan <dylansamuelwright@gmail.com>
2015-03-24 20:42:43 -0700

    working file upload

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -4,15 +4,17 @@
 // For "Select Profile Image" hides defalt chose file 
 // button and applies styled one
 
-$(document).ready(function() {
+$(document).on('change', '.btn-file :file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
 
-	$(document).on('change', '.btn-file :file', function() {
-	    var input = $(this);
-	    input.trigger('fileselect');
-	});
-
-    $('.btn-file :file').on('fileselect') {
-        $('input').html();
+$(document).ready( function() {
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+      console.log(numFiles);
+      console.log(label);
+      $('#signup-label').html(label);
     });
-
 });
