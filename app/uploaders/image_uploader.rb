30f85b809d0b5007d7c4f20892f942f4e5baa201
Dylan <dylansamuelwright@gmail.com>
2015-04-02 08:42:16 -0700

    updated with resize to fill

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -24,7 +24,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-    process :resize_to_fit => [300, 300]
+    process :resize_to_fill => [192, 192]
   #
   # def scale(width, height)
   #   # do something
