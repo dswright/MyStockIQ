d3b3da6de1bc909955372bc2167121ef79265e56
Dylan <dylansamuelwright@gmail.com>
2015-04-06 10:16:15 -0700

    migration update

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150402063429) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
