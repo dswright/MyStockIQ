fceca9b4a473102c0a95bc59003b69641a4dede6
Dylan <dylansamuelwright@gmail.com>
2015-04-30 22:11:11 -0700

    date string updater on helper

--- app/workers/helper_worker.rb
+++ app/workers/helper_worker.rb
@@ -30,56 +30,58 @@ class HelperWorker
   # end
 
   #this rounds all of the open and close price values to 2 decimals. Scraper has been updated.
-  def perform(ticker_symbol)
-    stockprices = Intradayprice.where(ticker_symbol:ticker_symbol)
-    case_line_opens = []
-    case_line_closes = []
-    stockprices.each do |stockprice|
-      case_line_opens << "WHEN date = '#{stockprice.date}' THEN #{stockprice.open_price.round(2)}"
-      case_line_closes << "WHEN date = '#{stockprice.date}' THEN #{stockprice.close_price.round(2)}"
-    end
+  # def perform(ticker_symbol)
+  #   stockprices = Intradayprice.where(ticker_symbol:ticker_symbol)
+  #   case_line_opens = []
+  #   case_line_closes = []
+  #   stockprices.each do |stockprice|
+  #     case_line_opens << "WHEN date = '#{stockprice.date}' THEN #{stockprice.open_price.round(2)}"
+  #     case_line_closes << "WHEN date = '#{stockprice.date}' THEN #{stockprice.close_price.round(2)}"
+  #   end
 
-    unless case_line_opens == []
-      sql = "update intradayprices
-              SET open_price = CASE
-                #{case_line_opens.join("\n")}
-              END
-            WHERE ticker_symbol = '#{ticker_symbol}';" #this is the sql shell that runs. Its contents are based on its 2 arrays.
-      ActiveRecord::Base.connection.execute(sql) #this executes the raw sql.
+  #   unless case_line_opens == []
+  #     sql = "update intradayprices
+  #             SET open_price = CASE
+  #               #{case_line_opens.join("\n")}
+  #             END
+  #           WHERE ticker_symbol = '#{ticker_symbol}';" #this is the sql shell that runs. Its contents are based on its 2 arrays.
+  #     ActiveRecord::Base.connection.execute(sql) #this executes the raw sql.
 
-      sql = "update intradayprices
-              SET close_price = CASE
-                #{case_line_closes.join("\n")}
+  #     sql = "update intradayprices
+  #             SET close_price = CASE
+  #               #{case_line_closes.join("\n")}
+  #             END
+  #           WHERE ticker_symbol = '#{ticker_symbol}';" #this is the sql shell that runs. Its contents are based on its 2 arrays.
+  #     ActiveRecord::Base.connection.execute(sql) #this executes the raw sql.
+  #   end
+  # end
+
+  def perform(ticker_symbol) #update the date string to be correct.
+    stockprices = Stockprice.where(ticker_symbol:ticker_symbol)
+    case_lines = []
+    tz = ActiveSupport::TimeZone.new('America/New_York')
+
+    unless stockprices.empty?
+
+      stockprices.each do |stockprice|
+        
+        old_date = stockprice.date
+        date_string = DateTime.strptime(old_date.to_s, "%Y-%m-%d")
+        new_time_string = Time.parse(date_string.to_s) #convert the date format to a time format so that utc_time_full can be used.
+        offset = tz.parse(new_time_string.to_s).utc_offset() #get the offset amount from EST. Could be 4 or 5 hours depending on DSt
+        new_date = new_time_string - offset + 16*3600 #create the final adjusted UTC time.
+
+        case_lines << "WHEN date = '#{stockprice.date}' THEN CAST('#{new_date}' AS timestamp)"
+      end
+
+      sql = "update stockprices
+              SET date = CASE
+                #{case_lines.join("\n")}
               END
             WHERE ticker_symbol = '#{ticker_symbol}';" #this is the sql shell that runs. Its contents are based on its 2 arrays.
       ActiveRecord::Base.connection.execute(sql) #this executes the raw sql.
     end
   end
-
-  # def perform(ticker_symbol) #update the date string to be correct.
-  #   stockprices = Stockprice.where(ticker_symbol:ticker_symbol)
-  #   case_lines = []
-  #   tz = ActiveSupport::TimeZone.new('America/New_York')
-
-  #   stockprices.each do |stockprice|
-      
-  #     old_date = stockprice.date
-  #     date_string = DateTime.strptime(old_date.to_s, "%Y-%m-%d")
-  #     new_time_string = Time.parse(date_string.to_s) #convert the date format to a time format so that utc_time_full can be used.
-  #     offset = tz.parse(new_time_string.to_s).utc_offset() #get the offset amount from EST. Could be 4 or 5 hours depending on DSt
-  #     new_date = new_time_string - offset + 16*3600 #create the final adjusted UTC time.
-
-  #     case_lines << "WHEN date = '#{stockprice.date}' THEN CAST('#{new_date}' AS timestamp)"
-  #   end
-
-  #   sql = "update stockprices
-  #           SET date = CASE
-  #             #{case_lines.join("\n")}
-  #           END
-  #         WHERE ticker_symbol = '#{ticker_symbol}';" #this is the sql shell that runs. Its contents are based on its 2 arrays.
-  #   ActiveRecord::Base.connection.execute(sql) #this executes the raw sql.
-
-  # end
 
 
   # def perform(ticker_symbol) #update the graph_time in the daily table. Delete intraday table to take care of those.
