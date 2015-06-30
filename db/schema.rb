dfc54139ad39c7bb598034ac648a9718aa78cb46
danajwright <danajwright@gmail.com>
2015-06-29 18:01:41 -0700

    database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150628001502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
 
   create_table "comments", force: true do |t|
     t.text     "content"
